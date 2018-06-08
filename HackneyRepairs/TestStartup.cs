0e890475be3c729ff21fa24a6e2b0908a6877f6b
Selwyn Preston <selwyn.preston@hackney.gov.uk>
2018-06-08 16:02:34 +0100

    Test environment variables added to the test startup class.

--- HackneyRepairs/TestStartup.cs
+++ HackneyRepairs/TestStartup.cs
@@ -1,4 +1,5 @@
-﻿using HackneyRepairs.Tests;
+﻿using System;
+using HackneyRepairs.Tests;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -12,6 +13,9 @@ namespace HackneyRepairs
         public TestStartup(IHostingEnvironment env) : base(env)
         {
             TestStatus.IsRunningInTests = true;
+            Environment.SetEnvironmentVariable("UhtDb", "connectionString=Test");
+            Environment.SetEnvironmentVariable("UhwDb", "connectionString=Test");
+            Environment.SetEnvironmentVariable("UhSorSupplierMapping", "08500820,H01|20040010,H01|20040020,H01|20040060,H01|20040310,H01|20060020,H01|20060030,H01|20110010,H01|48000000,H05|PRE00001,H02");
         }
     }
 }
