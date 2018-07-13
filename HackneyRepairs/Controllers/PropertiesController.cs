b72c53db63ef1ef52f44c2bf08381bb9bf1a3354
Selwyn Preston <selwyn.preston@hackney.gov.uk>
2018-07-13 14:53:13 +0100

    Added the endpoint to get the estate details for a property using the property reference number.

--- HackneyRepairs/Controllers/PropertiesController.cs
+++ HackneyRepairs/Controllers/PropertiesController.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using System.Net;
 using HackneyRepairs.Models;
 using HackneyRepairs.Interfaces;
 using HackneyRepairs.Factories;
@@ -135,7 +134,8 @@ namespace HackneyRepairs.Controllers
                 return jsonResponse;
             }
         }
-        // GET details of block by property by reference
+
+        // GET details of a property block by property by reference
         /// <summary>
         /// Gets the details of a block of a property by a given property reference number
         /// </summary>
@@ -177,6 +177,49 @@ namespace HackneyRepairs.Controllers
                 return jsonResponse; 
             }
 
+        }
+
+        // GET details of a property's estate by property by reference
+        /// <summary>
+        /// Gets the details of an estate of a property by a given property reference number
+        /// </summary>
+        /// <param name="reference">The reference number of the property</param>
+        /// <returns>Details of the estate the requested property belongs to</returns>
+        [HttpGet("{reference}/estate")]
+        public async Task<JsonResult> GetEstateByReference(string reference)
+        {
+            try
+            {
+                PropertyActions actions = new PropertyActions(_propertyService, _propertyServiceRequestBuilder, _loggerAdapter);
+                var result = await actions.FindPropertyEstateDetailsByRef(reference);
+                if (result == null)
+                {
+                    var jsonResponse = Json(null);
+                    jsonResponse.StatusCode = 404;
+                    return jsonResponse;
+                }
+                else
+                {
+                    var json = Json(result);
+                    json.StatusCode = 200;
+                    json.ContentType = "application/json";
+                    return json;
+                }
+            }
+            catch (Exception e)
+            {
+                var errors = new List<ApiErrorMessage>()
+                {
+                    new ApiErrorMessage
+                    {
+                        developerMessage = "API Internal Error",
+                        userMessage = "API Internal Error"
+                    }
+                };
+                var jsonResponse = Json(errors);
+                jsonResponse.StatusCode = 500;
+                return jsonResponse;
+            }
         }
     }
 }
