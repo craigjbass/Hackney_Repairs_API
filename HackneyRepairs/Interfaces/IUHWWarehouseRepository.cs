30b84c5c38fe7dc1d0da3dffad96c6b0ec98c50f
Selwyn Preston <selwyn.preston@hackney.gov.uk>
2018-07-18 15:46:34 +0100

    Removed a method signagure from the interface that is not part of this project.

--- HackneyRepairs/Interfaces/IUHWWarehouseRepository.cs
+++ HackneyRepairs/Interfaces/IUHWWarehouseRepository.cs
@@ -6,7 +6,6 @@ namespace HackneyRepairs.Interfaces
 {
     public interface IUHWWarehouseRepository
     {
-        Task<object> GetTagReferencenumber(string hackneyhomesId);
         Task<PropertySummary[]> GetPropertyListByPostCode(string post_code);
         Task<PropertyDetails> GetPropertyDetailsByReference(string reference);
         Task<PropertyDetails> GetPropertyBlockByReference(string reference);
