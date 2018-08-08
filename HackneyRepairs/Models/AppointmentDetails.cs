46991ca117602ffac03bfb249f08e8e82cc7a1a6
Miguel Saitz <miguel.saitz@hackney.gov.uk>
2018-08-08 13:43:37 +0100

    Add AppointmentDetails model and DRSAppointment entity

---
+++ HackneyRepairs/Models/AppointmentDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace HackneyRepairs.Models
+{
+    public class AppointmentDetails : Appointment
+    {
+        public DateTime TargetDate { get; set; }
+        public string AllocatedResource { get; set; }
+        public string Status { get; set; }
+        public string Priority { get; set; }
+        public string DataSource { get; set; }
+        // out of target attribute?
+    }
+}
