1534bb5a8ff60c294675e2fc7a4f8a2e16ea3bb5
Miguel Saitz <miguel.saitz@hackney.gov.uk>
2018-08-20 11:45:41 +0100

    note model created

---
+++ HackneyRepairs/Models/Note.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace HackneyRepairs.Models
+{
+    public class Note
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public DateTime LoggedAt { get; set; }
+        public string LoggedBy { get; set; }
+    }
+}
